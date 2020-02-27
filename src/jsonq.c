3a3044b8660c6fa02aa503b81129de32d6acc741
Joshua Weinstein <jweinst1@berkeley.edu>
2020-02-27 10:05:22 -0800

    json parser or query

---
+++ src/jsonq.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define JSONT_OSTART '{'
+#define JSONT_OEND '}'
+#define JSONT_ASTART '['
+#define JSONT_AEND ']'
+#define JSONT_STR '"'
+#define JSONT_COMMA ','
+#define JSONT_COLON ':'
+#define JSONT_TSTART 't'
+#define JSONT_FSTART 'f'
+#define JSONT_NSTART 'n'
+#define JSONT_D0 '0'
+#define JSONT_D1 '1'
+#define JSONT_D2 '2'
+#define JSONT_D3 '3'
+#define JSONT_D4 '4'
+#define JSONT_D5 '5'
+#define JSONT_D6 '6'
+#define JSONT_D7 '7'
+#define JSONT_D8 '8'
+#define JSONT_D9 '9'
+
+int main(void) {
+	printf("running: %s\n", __FILE__);
+	return 0;
+}
